de3454037a3db3856e73e6096d5cb638df29ddd8
Beka Dugladze <125121125+BekaDugladze@users.noreply.github.com>
2024-01-09 14:41:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 
 app.use(
     cors({
-        origin: 'https://spotify-app-front-five.vercel.app/',
+        origin: 'https://spotify-app-front-zeta.vercel.app',
         methods: 'GET, POST, PUT, DELETE',
         credentials: true,
     })
