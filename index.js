5f080ac3ea359f2ff8941e9273f29084756ec328
Beka Dugladze <125121125+BekaDugladze@users.noreply.github.com>
2024-01-29 13:42:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://spotify-app-front-zeta.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
